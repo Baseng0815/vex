cc77dbfb9e46b8fde57f7c7e08f5ddec8eb032b9
Bastian Engel <bastian.engel00@gmail.com>
2022-03-01 17:01:40 +0100

    add screen movement, beginning/end seeking and dynamic word size

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ hjkl | move left,down,up,right | yes
 w | move to next word | no
 b | move to beginning of word | no
 e | move to end of word | no
-\+ | increase word size | no
-\- | decrease word size | no
-g | go to beginning | no
-G | go to end | no
-0 | go to beginning of line | no
-^ | go to end of line | no
+U | move one screen up | yes
+D | move one screen down | yes
+g | go to beginning of file | no
+G | go to end of file | no
+0 | go to beginning of line | yes
+$ | go to end of line | yes
+\+ | increase word size | yes
+\- | decrease word size | yes
 : | seek offset | no
 m | set marker | no
 ' | goto marker | no
--- src/vex.c
+++ src/vex.c
@@ -10,6 +10,7 @@ struct vex_state {
         struct buffer *data;
         uint64_t offset_data;
         uint64_t offset_screen;
+        uint8_t word_size; // in bytes (1,2,4,8)
 } state;
 
 int ty, tx;
@@ -29,6 +30,7 @@ void vex_init(struct buffer *data) {
         state.data          = data;
         state.offset_data   = 0;
         state.offset_screen = 0;
+        state.word_size     = 1;
 
         initscr();
         noecho();
@@ -51,25 +53,34 @@ void vex_draw(void)
 
         char buf[24];
 
-        for (int byte = 0; byte < 16; byte++) {
-                sprintf(buf, "%02x ", byte);
-                mvprintw(0, 24 + 3 * byte, buf);
+        sprintf(buf, "one word = %d bytes", state.word_size);
+        mvprintw(0, 0, buf);
+
+        for (int bytei = 0; bytei < 16; bytei += state.word_size) {
+                sprintf(buf, "%02x ", bytei);
+                mvprintw(0, 24 + 2 * bytei + bytei / state.word_size, buf);
         }
 
         for (int y = 0; y < ty - 1; y++) {
                 uint64_t row_addr = state.offset_screen + 0x10 * y;
                 sprintf(buf, "0x%016lx", row_addr);
                 mvprintw(1 + y, 0, buf);
-                for (int x = 0; x < 16; x++) {
-                        uint64_t addr = row_addr + x;
+                int cx = 24;
+                for (int bytei = 0; bytei < 16; bytei++) {
+                        uint64_t addr = row_addr + bytei;
                         sprintf(buf, "%02x", vex_data_read(addr));
-                        mvprintw(1 + y, 24 + 3 * x, buf);
+                        mvprintw(1 + y, cx, buf);
+                        cx += 2;
+                        if ((bytei + 1) % state.word_size == 0) {
+                                cx++;
+                        }
                 }
         }
 
         uint64_t relative_screen_addr = state.offset_data - state.offset_screen;
-        int cy = 1 + ROUND_DOWN(relative_screen_addr, 0x10) / 0x10;
-        int cx = 24 + 3 * (relative_screen_addr % 0x10);
+        int cy = 1 + relative_screen_addr / 0x10;
+        uint8_t bytei = relative_screen_addr & 0xf;
+        int cx = 24 + 2 * bytei + bytei / state.word_size;
         move(cy, cx);
 }
 
@@ -93,10 +104,35 @@ void vex_loop(void)
                         case 'l':
                                 vex_change_offset(1);
                                 break;
+                        case 'U':
+                                vex_change_offset(-screen_bytes);
+                                break;
+                        case 'D':
+                                vex_change_offset(screen_bytes);
+                                break;
+                        case '0':
+                                state.offset_data =
+                                        ROUND_DOWN(state.offset_data, 0x10);
+                                break;
+                        case '$':
+                                state.offset_data =
+                                        ROUND_DOWN(state.offset_data + 0x10,
+                                                   0x10) - 1;
+                                break;
+                        case '+':
+                                if (state.word_size < 8) {
+                                        state.word_size <<= 1;
+                                }
+                                break;
+                        case '-':
+                                if (state.word_size > 1) {
+                                        state.word_size >>= 1;
+                                }
                         default:
                                 break;
                 }
 
+                clear();
                 vex_draw();
                 refresh();
         }
